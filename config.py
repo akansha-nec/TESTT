77fbd191938ee35717bea860c37b6d32d51aa17c
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-10-29 16:24:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,35 @@
+# Configuration files are a really frequent source of secrets
+
+'''Common Key Types with obvious names'''
+
+# AWS
+## AWS Access Key ID
+AWS_ACCESS_KEY_ID = 'AKIAIWSXFHRM7F6Z3NWQ'
+
+## AWS Secret Access Key
+AWS_ACCESS_SECRET_KEY = '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'
+
+## AWS MWS Auth Token
+MWS_AUTH_TOKEN = 'amzn.mws.f90f3ce6-9b5a-26a7-9a87-4ff8052be2ec'
+
+- text: >
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    CLIENT SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+  client_id: A3T6AKIAFJKR45SAWS5Z
+  Sec: hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+
+- text: |
+    //545084392359.signin.aws.amazon.com/console
+    +
+    +
+    +AKIAX52MPYOTPRUCRC22
+    +9rtYl+xkeUhTBuOnnUuXRllSipaqGSq5WlL+NBwu
+    +
+    +===========================
+    +Microservices Co
+  client_id: AKIAX52MPYOTPRUCRC22
+  myfile: 9rtYl+xkeUhTBuOnnUuXRllSipaqGSq5WlL+NBwu
+
+
+  client_id: AKIAIX4ONRSG6ODEFVJA
+  __: aAUFXbrEuFC8V8c10vk8AVXIt5TjIbKpUZ9IPc/a
